522be0049b8aa6d7e2ee91511882939bf0c15c93
ekat1983 <shumkovakatya@yandex.ru>
2022-04-29 22:16:06 +0300

    'init'

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+
+    public int calkulate(int cost) {
+        int bonus = cost / 20;
+
+        // System.out.println("cумма покупки:" + price + "руб." + "руб.");
+        //System.out.println("Ваш бонуc:" + bonus + " миль");
+        return bonus;
+
+    }
+}
